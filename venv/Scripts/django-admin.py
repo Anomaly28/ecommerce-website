83a55ec49bf0c5e52e48ec2641876f4fe797a45f
Anomaly28 <Nidhibhawari28@gmail.com>
2025-04-22 22:21:46 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\NIDHI\Documents\python\ecommerce-master\ecommerce-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
